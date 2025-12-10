93322994f5b3569b80746ef5a8270b66c67aedd2
Marília Mariano <mahmariano240@gmail.com>
2025-12-10 19:55:30 -0300

    Update image selector in lightbox functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
+// CAROUSEL
+document.querySelectorAll(".produto-carousel").forEach(carousel => {
+  const track = carousel.querySelector(".carousel-track");
+  const prev = carousel.querySelector(".prev");
+  const next = carousel.querySelector(".next");
+  
+  prev.addEventListener("click", () => track.scrollBy({left:-120, behavior:"smooth"}));
+  next.addEventListener("click", () => track.scrollBy({left:120, behavior:"smooth"}));
+});
+
+// LIGHTBOX
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
 const closeBtn = document.querySelector(".close");
 
-document.querySelectorAll(".produto-gallery img.thumb").forEach(img => {
+document.querySelectorAll(".produto-carousel img.thumb").forEach(img => {
   img.addEventListener("click", () => {
     modal.style.display = "block";
     modalImg.src = img.src;

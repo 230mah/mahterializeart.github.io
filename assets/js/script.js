141ccbfb79c2326c54b24622c54015e62771183d
Marília Mariano <mahmariano240@gmail.com>
2025-12-15 18:10:24 -0300

    Refactor modal image gallery event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
 const close = document.getElementById("close");
 
-document.querySelectorAll(".galeria img").forEach(img => {
+document.querySelectorAll(".thumb").forEach(img => {
   img.addEventListener("click", () => {
     modal.style.display = "flex";
     modalImg.src = img.src;
@@ -10,5 +10,6 @@ document.querySelectorAll(".galeria img").forEach(img => {
 });
 
 close.onclick = () => modal.style.display = "none";
-modal.onclick = e => { if (e.target === modal) modal.style.display = "none"; }
-
+modal.onclick = e => {
+  if (e.target === modal) modal.style.display = "none";
+};

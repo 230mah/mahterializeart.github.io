e64275140facdca04c36e96bc667920d2949dd43
Marília Mariano <mahmariano240@gmail.com>
2025-12-15 18:55:29 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
-const close = document.getElementById("close");
 
-document.querySelectorAll(".thumb").forEach(img=>{
-  img.addEventListener("click",()=>{
+document.querySelectorAll(".galeria img").forEach(img=>{
+  img.onclick = ()=>{
     modal.style.display="flex";
     modalImg.src=img.src;
-  });
+  }
 });
 
-close.onclick=()=>modal.style.display="none";
-modal.onclick=e=>{if(e.target===modal)modal.style.display="none";}
+modal.onclick = ()=> modal.style.display="none";

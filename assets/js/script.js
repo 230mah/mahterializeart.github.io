75ebb7064b30b4620ab6a98b00df0c6e8d262253
Marília Mariano <mahmariano240@gmail.com>
2025-12-15 17:45:36 -0300

    Rename 'fechar' to 'close' and update event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
-const fechar = document.getElementById("fechar");
+const close = document.getElementById("close");
 
-document.querySelectorAll(".thumb").forEach(img=>{
-  img.addEventListener("click", ()=>{
+document.querySelectorAll(".galeria img").forEach(img => {
+  img.addEventListener("click", () => {
     modal.style.display = "flex";
     modalImg.src = img.src;
   });
 });
 
-fechar.onclick = ()=> modal.style.display = "none";
-
-modal.onclick = e=>{
-  if(e.target === modal){
-    modal.style.display = "none";
-  }
-};
-
+close.onclick = () => modal.style.display = "none";
+modal.onclick = e => { if (e.target === modal) modal.style.display = "none"; }
 
